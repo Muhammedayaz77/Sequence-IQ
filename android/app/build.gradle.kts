06666d2d8ca05ee27fce55f84ef888449a7748ad
Mohammed Ayaz <ayaz.job2010@gmail.com>
2026-09-10 21:40:25 +0530

    Add native Android application build configuration

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+
+android { namespace = "com.hindtechgroup.sequenceiq"; compileSdk = 36
+    defaultConfig { applicationId = "com.hindtechgroup.sequenceiq"; minSdk = 26; targetSdk = 36; versionCode = 1; versionName = "1.0" }
+}
+
+kotlin { jvmToolchain(17) }
+
+android.sourceSets["main"].java.srcDirs("..", "../View", "../Controller", "../Model", "../Helper")
+
